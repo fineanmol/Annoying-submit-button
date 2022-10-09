33128a43250842d26f71f58730130803256c0779
fineanmolwork <anmol.agarwal@peopleinteractive.in>
2022-10-09 22:34:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import {React,useState} from 'react';
-import './App.css';
-import Footer from './components/Footer';
-import ThemeButton from './components/ThemeButton'
-
 import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import ThemeButton from "./components/ThemeButton";
 
-
-
-
 function App() {
-
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-  });
-
-  const [toggleClass, setToggleClass] = useState(false);
-  const [showToast, setShowToast] = useState(false);
-  const [themeState, setThemeState] = useState("dark")
-  const [isDark,setDark] = useState(true);
- 
-  //styling the border dynamically
-  const style = {
-    border:`2px solid ${isDark ? "white" : "black"}`,
-    borderRadius : "5px"
-  }
-
   const [form, setForm] = React.useState({
     email: "",
     password: "",
@@ -38,10 +13,7 @@ function App() {
   const [showToast, setShowToast] = React.useState(false);
   const [themeState, setThemeState] = React.useState("dark");
 
-
-  console.log(style)
   const handleForm = (e) => {
-    e.preventDefault();
     setForm((formProps) => ({ ...formProps, [e.target.name]: e.target.value }));
   };
 
@@ -67,17 +39,18 @@ function App() {
 
   return (
     <>
-
-      <ThemeButton 
-      setThemeState={setThemeState}
-       themeState={themeState}
-       setDark={setDark}
-       />
-
       <ThemeButton setThemeState={setThemeState} themeState={themeState} />
-
       <section className={`form-section ${themeState}-theme`}>
-        <h1 className="heading">Annoying Submit Button 😡🙃</h1>
+
+        <a href="#" className="link">
+          <span className="mask">
+            <div className="link-container">
+              <span className="link-title1 title">Annoying Submit Button 😡🙃</span>
+              <span className="link-title2 title">Annoying Submit Button 😡🙃</span>
+            </div>
+          </span>
+        </a>
+        
         <form
           autoComplete="false"
           action="https://formspree.io/f/xqkjbjzw"
@@ -88,8 +61,9 @@ function App() {
               Email <span className="requiredLabel">*</span>
             </label>
             <input
-              className={`input ${themeState}-theme ${!validateEmail(form.email) ? 'wrong-input' : 'correct-input'
-                }`}
+              className={`input ${themeState}-theme ${
+                !validateEmail(form.email) ? "wrong-input" : "correct-input"
+              }`}
               type="email"
               name="email"
               value={form.email}
@@ -101,11 +75,9 @@ function App() {
           </div>
           <div>
             {!validateEmail(form.email) ? (
-              <p className="warning-message">
-                Enter a valid email id
-              </p>
+              <p className="warning-message">Enter a valid email id</p>
             ) : (
-              ''
+              ""
             )}
           </div>
           <div className="input-block">
@@ -113,8 +85,9 @@ function App() {
               Password <span className="requiredLabel">*</span>
             </label>
             <input
-              className={`input ${form.password.length <= 6 ? 'wrong-input' : 'correct-input'
-                } ${themeState}-theme`}
+              className={`input ${
+                form.password.length <= 6 ? "wrong-input" : "correct-input"
+              } ${themeState}-theme`}
               type="password"
               name="password"
               value={form.password}
@@ -130,11 +103,20 @@ function App() {
                 Password length should be more than 6
               </p>
             ) : (
-              ''
+              ""
             )}
           </div>
           <div
-            className={`submit-button-wrapper ${toggleClass ? 'float-end' : 'float-start'}`}
+            style={{
+              transform: `translateX(${
+                toggleClass &&
+                !(form.password.length > 6 && validateEmail(form.email))
+                  ? "25vh"
+                  : "0"
+              }`,
+              transition: "transform 190ms ease-in-out",
+            }}
+            // className={`submit-button-wrapper ${toggleClass ? 'float-end' : 'float-start'}`}
           >
             <button
               tabIndex={-1}
@@ -151,7 +133,11 @@ function App() {
               Submit
             </button>
           </div>
-          <div className={`toast ${showToast ? 'fadeIn' : 'fadeOut'} ${themeState}-theme-toast`}>
+          <div
+            className={`toast ${
+              showToast ? "fadeIn" : "fadeOut"
+            } ${themeState}-theme-toast`}
+          >
             You can not submit until you fix all the validation errors...
           </div>
         </form>
