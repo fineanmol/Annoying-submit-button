3a00b400b0cc4d3705976d9a56acc7fb519cadf3
prateekpranveer <prateekpranveer321@gmail.com>
2022-10-10 12:54:46 -0700

    added multi-theme

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -1,28 +1,37 @@
-import React, {useState} from "react";
+import React from "react";
 import "./ThemeButton.css";
 
 const ThemeButton = ({ setThemeState, themeState }) => {
-
   return (
     <div>
-        <div className='theme-button-container'>
-
-            <div className={`theme-btn purple ${themeState}-d`} onClick={() => setThemeState("purple")}>
-              <div className="tick"></div>
-            </div>
-            <div className={`theme-btn pink ${themeState}-c`} onClick={() => setThemeState("pink")}>
-            <div className="tick"></div>
-            </div>
-            <div className={`theme-btn skin ${themeState}-b`} onClick={() => setThemeState("skin")}>
-            <div className="tick"></div>
-            </div>
-            <div className={`theme-btn dark ${themeState}-a`} onClick={() => setThemeState("dark")}>
-            <div className="tick"></div>
-            </div>
-    
+      <div className="theme-button-container">
+        <div
+          className={`theme-btn purple ${themeState}-d`}
+          onClick={() => setThemeState("purple")}
+        >
+          <div className="tick"></div>
         </div>
+        <div
+          className={`theme-btn pink ${themeState}-c`}
+          onClick={() => setThemeState("pink")}
+        >
+          <div className="tick"></div>
+        </div>
+        <div
+          className={`theme-btn skin ${themeState}-b`}
+          onClick={() => setThemeState("skin")}
+        >
+          <div className="tick"></div>
+        </div>
+        <div
+          className={`theme-btn dark ${themeState}-a`}
+          onClick={() => setThemeState("dark")}
+        >
+          <div className="tick"></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default ThemeButton;
