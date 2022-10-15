88d356538f0c284095cb0fb6d9f86baa82ea8ba3
Shubhankar <shubhankar.nautiyal21@gmail.com>
2022-10-15 19:08:20 +0530

    Added green theme

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import "./ThemeButton.css";
 
 const ThemeButton = ({ setThemeState, themeState }) => {
-  const themeNamesArr = ["purple", "pink", "skin", "dark"];
-  return (
-    <div>
-      <div className="theme-button-container">
-        {themeNamesArr.map((el) => (
-          <div
-            key={el}
-            className={`theme-btn ${el} ${themeState}-d`}
-            onClick={() => setThemeState(el)}
-          >
-            <div className="tick"></div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    const themeNamesArr = ["purple", "pink", "skin", "green", "dark"];
+    return (
+        <div>
+            <div className="theme-button-container">
+                {themeNamesArr.map((el) => (
+                    <div
+                        key={el}
+                        className={`theme-btn ${el} ${themeState}-d`}
+                        onClick={() => setThemeState(el)}>
+                        <div className="tick"></div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default ThemeButton;
