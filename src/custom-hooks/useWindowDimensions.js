e991ffaa4cc1bac29326568c69219226956a6c42
Arjun Sethi <63541672+fishmeister09@users.noreply.github.com>
2022-10-21 20:14:37 +0530

    Update useWindowDimensions.js

--- src/custom-hooks/useWindowDimensions.js
+++ src/custom-hooks/useWindowDimensions.js
@@ -1,26 +1,23 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react'
 
 function getWindowDimensions() {
-  const {innerWidth: width, innerHeight: height} = window;
-  return {
-    width,
-    height,
-  };
+  const { innerWidth: width, innerHeight: height } = window
+  return { width, height }
 }
 
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
+    getWindowDimensions(),
+  )
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowDimensions(getWindowDimensions())
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
-  return windowDimensions;
+  return windowDimensions
 }
