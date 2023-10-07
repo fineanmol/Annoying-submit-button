857c7ba16a35af078cfd5d8a42cd8b6c61e6418d
Piumi Rathnayake <73763694+Piumir3@users.noreply.github.com>
2023-10-07 16:47:07 +0530

    Update App.js for previous error messages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import Footer from './components/Footer'
-import ThemeButton from './components/ThemeButton'
-import useWindowDimensions from './custom-hooks/useWindowDimensions'
-// ...
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import ThemeButton from './components/ThemeButton';
+import useWindowDimensions from './custom-hooks/useWindowDimensions';
 
 const App = () => {
   // ... (existing code)
+  
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     setShowToast(false);
 
-    if (
-      form.password.length < minPasswordLength ||
-      !validateEmail(form.email)
-    ) {
-      setToggleClass((prevState) => !prevState);
+    setEmailError(''); // Clear previous error messages
+    setPasswordError('');
+
+    if (form.password.length < minPasswordLength) {
+      setPasswordError('Password must be at least ' + minPasswordLength + ' characters long.');
+    }
+
+    if (!validateEmail(form.email)) {
+      setEmailError('Please enter a valid email address.');
+    }
+
+    if (passwordError || emailError) {
       setShowToast(true);
       setTimeout(() => {
         setShowToast(false);
@@ -49,7 +58,33 @@ const App = () => {
         method="POST"
         onSubmit={handleFormSubmit}
       >
-        {/* ... (existing code) */}
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={form.email}
+            onChange={handleInputChange}
+            required
+          />
+          {emailError && <div className="error">{emailError}</div>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={form.password}
+            onChange={handleInputChange}
+            required
+          />
+          {passwordError && <div className="error">{passwordError}</div>}
+        </div>
+
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
